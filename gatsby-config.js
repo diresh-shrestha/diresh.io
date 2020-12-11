1446165241c3b11197ccb6fc400413cd326191dd
diresh-shrestha <direshshrestha95@gmail.com>
2020-12-10 23:21:12 -0600

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,17 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-X1YD0WVKDS", // Google Analytics / GA
+          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
+    {
       resolve: "gatsby-source-goodreads",
       options: {
         developerKey: "n64tyIdRZf7Ymjj0rULGow",
