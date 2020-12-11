665a63914332e2c62f43c82804a757ebc5b6cfe8
diresh-shrestha <direshshrestha95@gmail.com>
2020-12-10 23:41:44 -0600

    added sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     {
