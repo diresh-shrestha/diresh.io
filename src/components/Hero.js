5ac56faa83efdf228524bb9b62bb818da31ffff2
diresh-shrestha <direshshrestha95@gmail.com>
2021-01-10 12:29:08 -0600

    removed unused js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,5 @@
 import React from "react"
 import styled from "styled-components"
-import Typer from "./Typer"
 import Img from "gatsby-image"
 import Container from "./Container"
 import SocialLinks from "./icons/SocialLinks"
