f4ee0a6ed7e040667562c8d6a1813c5c25e7a992
diresh-shrestha <direshshrestha95@gmail.com>
2021-02-14 14:06:56 -0600

    bmc test

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,22 @@
  */
 
 // You can delete this file if you're not using it
+
+const React = require("react")
+
+exports.onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script
+      data-name="bmc-button"
+      data-slug="diresh"
+      src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js"
+      data-color="#FF5F5F"
+      data-emoji=""
+      data-font="Cookie"
+      data-text="Buy me a coffee"
+      data-outline-color="#000000"
+      data-font-color="#ffffff"
+      data-coffee-color="#FFDD00"
+    ></script>,
+  ])
+}
