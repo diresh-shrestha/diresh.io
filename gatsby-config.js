25648e2e9c68157195d22707ec609a6d61bf76a2
diresh-shrestha <direshshrestha95@gmail.com>
2021-02-20 19:09:39 -0600

    uninstalled unused libraries

--- gatsby-config.js
+++ gatsby-config.js
@@ -176,27 +176,12 @@ module.exports = {
         showSpinner: true,
       },
     },
-    `gatsby-plugin-scroll-reveal`,
+
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     "gatsby-theme-elements",
     "gatsby-plugin-dark-mode",
-    {
-      resolve: `gatsby-plugin-scroll-reveal`,
-      options: {
-        threshold: 0.5, // Percentage of an element's area that needs to be visible to launch animation
-        once: true, // Defines if animation needs to be launched once
-        disable: false, // Flag for disabling animations
 
-        // Advanced Options
-        selector: "[data-sal]", // Selector of the elements to be animated
-        animateClassName: "sal-animate", // Class name which triggers animation
-        disabledClassName: "sal-disabled", // Class name which defines the disabled state
-        rootMargin: "0% 50%", // Corresponds to root's bounding box margin
-        enterEventName: "sal:in", // Enter event name
-        exitEventName: "sal:out", // Exit event name
-      },
-    },
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
