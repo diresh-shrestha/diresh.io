10292493c4072bc96eb5c4e4a2c196ef88fc428c
diresh-shrestha <direshshrestha95@gmail.com>
2021-02-24 20:53:05 -0600

    query fix

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -94,10 +94,9 @@ export default function BlogPost({ data }) {
   const components = {
     pre: CodeBlock,
   }
-
+  const image = data.mdx.frontmatter.image.childImageSharp.resize
   const siteUrl = "https://www.diresh.io/"
   const post = data.mdx
-  const image = post.frontmatter.image.childImageSharp.resize
   let disqusConfig = {
     url: `${siteUrl + post.frontmatter.slug}`,
     identifier: post.id,
